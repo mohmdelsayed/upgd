fd51cf4488062c15a669cfb3ee2bb3993dac6773
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-11-30 21:16:50 -0700

    normalize utility

--- optim/search/first_order.py
+++ optim/search/first_order.py
@@ -1,9 +1,10 @@
 import torch
+from torch.nn import functional as F
 
 # Utility-based Search Optimizers
 
 class FirstOrderSearchNormal(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.99, temp=1.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderSearchNormal, self).__init__(params, defaults)
 
@@ -19,10 +20,10 @@ class FirstOrderSearchNormal(torch.optim.Optimizer):
                 noise = torch.randn_like(p.grad)
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(noise * (1-torch.tanh_((avg_utility / bias_correction) / group["temp"])), alpha=-group["lr"])
+                p.data.add_(noise * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
 
 class FirstOrderSearchAntiCorr(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.99, temp=1.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderSearchAntiCorr, self).__init__(params, defaults)
 
@@ -41,4 +42,4 @@ class FirstOrderSearchAntiCorr(torch.optim.Optimizer):
                 state["prev_noise"] = new_noise
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(noise * (1-torch.tanh_((avg_utility / bias_correction) / group["temp"])), alpha=-group["lr"])
+                p.data.add_(noise * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
