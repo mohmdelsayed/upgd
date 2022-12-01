9b90ad0d446f4fff7f303704d75579637691cbf7
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-11-30 17:27:17 -0700

    fix bias correction bug

--- optim/search/first_order.py
+++ optim/search/first_order.py
@@ -3,7 +3,7 @@ import torch
 # Utility-based Search Optimizers
 
 class FirstOrderSearchNormal(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=1.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.99, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderSearchNormal, self).__init__(params, defaults)
 
@@ -19,10 +19,10 @@ class FirstOrderSearchNormal(torch.optim.Optimizer):
                 noise = torch.randn_like(p.grad)
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(noise * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
+                p.data.add_(noise * (1-torch.tanh_((avg_utility / bias_correction) / group["temp"])), alpha=-group["lr"])
 
 class FirstOrderSearchAntiCorr(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=1.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.99, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderSearchAntiCorr, self).__init__(params, defaults)
 
@@ -41,4 +41,4 @@ class FirstOrderSearchAntiCorr(torch.optim.Optimizer):
                 state["prev_noise"] = new_noise
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(noise * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
+                p.data.add_(noise * (1-torch.tanh_((avg_utility / bias_correction) / group["temp"])), alpha=-group["lr"])
