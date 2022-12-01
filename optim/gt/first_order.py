c6e2142bce1a0d086c8e0e17d19eb048098f1568
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-11-30 21:16:28 -0700

    fix bias correction bug

--- optim/gt/first_order.py
+++ optim/gt/first_order.py
@@ -1,8 +1,9 @@
 import torch
+from torch.nn import functional as F
 
 # UPGD: Utilited-based Perturbed Gradient Descent: variation 1 (utility doesn't control gradient)
 class FirstOrderUPGDv1Normal(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=5.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderUPGDv1Normal, self).__init__(params, defaults)
     def step(self):
@@ -17,11 +18,11 @@ class FirstOrderUPGDv1Normal(torch.optim.Optimizer):
                 avg_utility = state["avg_utility"]
                 noise = torch.randn_like(p.grad)
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(p.grad.data + noise * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
+                p.data.add_(p.grad.data + noise * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
 
 # UPGD: Utilited-based Perturbed Gradient Descent: variation 1 with Anti-correlated noise 
 class FirstOrderUPGDv1AntiCorr(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=5.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderUPGDv1AntiCorr, self).__init__(params, defaults)
     def step(self):
@@ -39,11 +40,11 @@ class FirstOrderUPGDv1AntiCorr(torch.optim.Optimizer):
                 state["prev_noise"] = new_noise
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_(p.grad.data + noise * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
+                p.data.add_(p.grad.data + noise * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
 
 # UPGD: Utilited-based Perturbed Gradient Descent: variation 2 (utility controls gradient)
 class FirstOrderUPGDv2Normal(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=1.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderUPGDv2Normal, self).__init__(params, defaults)
     def step(self):
@@ -58,11 +59,11 @@ class FirstOrderUPGDv2Normal(torch.optim.Optimizer):
                 avg_utility = state["avg_utility"]
                 noise = torch.randn_like(p.grad)
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_((p.grad.data + noise) * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
+                p.data.add_((p.grad.data + noise) * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
 
 # UPGD: Utilited-based Perturbed Gradient Descent: variation 2 with Anti-correlated noise 
 class FirstOrderUPGDv2AntiCorr(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, beta_utility=0.999, temp=5.0):
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0):
         defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp)
         super(FirstOrderUPGDv2AntiCorr, self).__init__(params, defaults)
     def step(self):
@@ -80,12 +81,12 @@ class FirstOrderUPGDv2AntiCorr(torch.optim.Optimizer):
                 state["prev_noise"] = new_noise
                 avg_utility = state["avg_utility"]
                 avg_utility.mul_(group["beta_utility"]).add_(-p.grad.data * p.data, alpha=1 - group["beta_utility"])
-                p.data.add_((p.grad.data + noise) * (1-torch.tanh_(avg_utility / group["temp"])), alpha=-group["lr"] / bias_correction)
-                
+                p.data.add_((p.grad.data + noise) * (1-torch.tanh_(F.normalize(avg_utility / bias_correction, dim=-1) / group["temp"])), alpha=-group["lr"])
+
 
 # UGD: Utility-regularized Gradient Descent
 class FirstOrderUGD(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5, lamda=1.0, beta_utility=0.999, beta_weight=0.999):
+    def __init__(self, params, lr=1e-5, lamda=1.0, beta_utility=0.0, beta_weight=0.9):
         defaults = dict(lr=lr, lamda=lamda, beta_utility=beta_utility, beta_weight=beta_weight)
         super(FirstOrderUGD, self).__init__(params, defaults)
     def step(self):
