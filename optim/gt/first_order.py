6ad9ab9751423a5c40f8b215cfa13b1c74d237ec
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-24 22:21:49 -0700

    add FirstOrderUGDLearnables optimizer

--- optim/gt/first_order.py
+++ optim/gt/first_order.py
@@ -197,3 +197,26 @@ class FirstOrderUGD(torch.optim.Optimizer):
                 avg_utility.mul_(group["beta_utility"]).add_(utility, alpha=1 - group["beta_utility"])
                 avg_weight.mul_(group["beta_weight"]).add_(p.data, alpha=1 - group["beta_weight"])
                 p.data.add_(p.grad.data + group["lamda"] * avg_utility * (p.data - avg_weight), alpha=-group["lr"] / bias_correction)
+
+# UGD: Utility-regularized Gradient Descent
+class FirstOrderUGDLearnables(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-5, lamda=1.0, beta_utility=0.9, beta_weight=0.9):
+        defaults = dict(lr=lr, lamda=lamda, beta_utility=beta_utility, beta_weight=beta_weight)
+        super(FirstOrderUGDLearnables, self).__init__(params, defaults)
+    def step(self, loss):
+        for group in self.param_groups:
+            for p in group["params"]:
+                state = self.state[p]
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["avg_utility"] = torch.zeros_like(p.data)
+                    state["avg_weight"] = torch.zeros_like(p.data)
+                    state["prev_weight"] = torch.zeros_like(p.data)
+                state["step"] += 1
+                bias_correction = 1 - group["beta_utility"] ** state["step"]
+                avg_utility = state["avg_utility"]
+                avg_weight = state["avg_weight"]
+                utility = - p.grad.data * (p.data - state["prev_weight"])
+                avg_utility.mul_(group["beta_utility"]).add_(utility, alpha=1 - group["beta_utility"])
+                avg_weight.mul_(group["beta_weight"]).add_(p.data, alpha=1 - group["beta_weight"])
+                p.data.add_(p.grad.data + group["lamda"] * avg_utility * (p.data - avg_weight), alpha=-group["lr"] / bias_correction)
