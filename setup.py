afd8c334b1bfc62140b30d9b2f31fa12a4a9eee9
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-24 22:21:32 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UPGD",
+    version="1.0.0",
+    description="Continual Perturbation with Utility Propagation",
+    url="https://github.com/mohmdelsayed",
+    author="Mohamed Elsayed",
+    author_email="mohamedelsayed@ualberta.ca",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=['backpack-for-pytorch==1.3.0'],
+)
