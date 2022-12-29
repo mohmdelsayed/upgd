a8b39dacd5055b177398997ff7278a4a9267c8bb
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 19:09:24 -0700

    make learner support extended networks to work with HesScale

--- core/learner/learner.py
+++ core/learner/learner.py
@@ -1,5 +1,6 @@
+from backpack import extend
 class Learner:
-    def __init__(self, name, network, optimizer, optim_kwargs):
+    def __init__(self, name, network, optimizer, optim_kwargs, extend=True):
         self.network_cls = network
         self.optim_kwargs = optim_kwargs
         for k, v in optim_kwargs.items():
@@ -7,6 +8,7 @@ class Learner:
                 optim_kwargs[k] = float(v)
         self.optimizer = optimizer
         self.name = name
+        self.extend = extend
 
     def __str__(self) -> str:
         return self.name
@@ -16,5 +18,8 @@ class Learner:
         return output
 
     def set_task(self, task):
-        self.network = self.network_cls(n_obs=task.n_inputs, n_outputs=task.n_outputs)
+        if self.extend:
+            self.network = extend(self.network_cls(n_obs=task.n_inputs, n_outputs=task.n_outputs))
+        else:
+            self.network = self.network_cls(n_obs=task.n_inputs, n_outputs=task.n_outputs)
         self.parameters = self.network.parameters()
