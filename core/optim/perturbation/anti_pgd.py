92d7b36dcff2795342edbc97dab921eff93521b1
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-29 11:54:38 -0700

    add sigma parameter in pgd and anti_pgd

--- core/optim/perturbation/anti_pgd.py
+++ core/optim/perturbation/anti_pgd.py
@@ -2,8 +2,8 @@ import torch
 
 
 class ExtendedAntiPGD(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5):
-        defaults = dict(lr=lr)
+    def __init__(self, params, lr=1e-5, sigma=1.0):
+        defaults = dict(lr=lr, sigma=sigma)
         super(ExtendedAntiPGD, self).__init__(params, defaults)
 
     def step(self, loss):
@@ -12,7 +12,7 @@ class ExtendedAntiPGD(torch.optim.Optimizer):
                 state = self.state[p]
                 # State initialization
                 if len(state) == 0:
-                    state["prev_noise"] = torch.zeros_like(p.data)
+                    state["prev_noise"] = torch.zeros_like(p.data) * group["sigma"]
 
                 new_noise = torch.randn_like(p.grad)
                 perturbed_gradient = p.grad + (new_noise - state["prev_noise"])
--- core/optim/perturbation/pgd.py
+++ core/optim/perturbation/pgd.py
@@ -2,12 +2,12 @@ import torch
 
 
 class ExtendedPGD(torch.optim.Optimizer):
-    def __init__(self, params, lr=1e-5):
-        defaults = dict(lr=lr)
+    def __init__(self, params, lr=1e-5, sigma=1.0):
+        defaults = dict(lr=lr, sigma=sigma)
         super(ExtendedPGD, self).__init__(params, defaults)
 
     def step(self, loss):
         for group in self.param_groups:
             for p in group["params"]:
-                perturbed_gradient = p.grad + torch.randn_like(p.grad)
+                perturbed_gradient = p.grad + torch.randn_like(p.grad) * group["sigma"]
                 p.data.add_(perturbed_gradient, alpha=-group["lr"])
