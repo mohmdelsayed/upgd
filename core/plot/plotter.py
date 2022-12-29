6d67d94d33d793501010e29e628e359c98cdefa1
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 21:01:20 -0700

    fix bug in plotting last n tasks for plotter

--- core/plot/plotter.py
+++ core/plot/plotter.py
@@ -49,7 +49,7 @@ class Plotter:
             plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
             plt.legend()
         
-        plt.xlabel(f"Task")
+        plt.xlabel(f"Time Step")
         plt.ylabel("Loss")
         plt.show()
 
@@ -63,25 +63,27 @@ class Plotter:
                     configuration_list.append(data["losses"])
                     learner_name = data["learner"]
 
-            mean_list = np.array(configuration_list).mean(axis=0)[-n_tasks * self.avg_interval-1:]
-            std_list = np.array(configuration_list).std(axis=0)[-n_tasks * self.avg_interval-1:] / np.sqrt(len(seeds))
-            plt.plot(mean_list, label=learner_name)
-            plt.title(f"First {n_tasks} tasks {self.task_name}")
-            plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
+            all_mean_list = np.array(configuration_list).mean(axis=0)
+            mean_list = all_mean_list[(-n_tasks * self.avg_interval):]
+            std_list = np.array(configuration_list).std(axis=0)[(-n_tasks * self.avg_interval):] / np.sqrt(len(seeds))
+            plt.plot(np.arange(n_tasks * self.avg_interval) + len(np.array(all_mean_list))  - n_tasks * self.avg_interval, mean_list, label=learner_name)
+            plt.title(f"Last {n_tasks} tasks {self.task_name}")
+            plt.fill_between(np.arange(n_tasks * self.avg_interval) + len(np.array(all_mean_list))  - n_tasks * self.avg_interval, mean_list - std_list, mean_list + std_list, alpha=0.1)
             plt.legend()
         
-        plt.xlabel(f"Task")
+        plt.xlabel(f"Time Step")
         plt.ylabel("Loss")
         plt.show()
 
 
 if __name__ == "__main__":
     # best_runs = BestRun("summer_with_sign_change", "area", "fully_connected_tanh", ["sgd", "upgdv2_normalized"]).get_best_run()
-    # best_runs = BestRun("summer_with_sign_change", "area", "fully_connected_linear", ["sgd", "upgdv2_normalized"]).get_best_run()
-    best_runs = BestRun("summer_with_signals_change", "area", "fully_connected_tanh", ["sgd", "upgdv2_normalized"]).get_best_run()
+    best_runs = BestRun("summer_with_sign_change", "area", "fully_connected_linear", ["sgd", "upgdv2_normalized_fo", "upgdv2_normalized_so"]).get_best_run()
+    # best_runs = BestRun("summer_with_signals_change", "area", "fully_connected_tanh", ["sgd", "upgdv2_normalized"]).get_best_run()
+    # best_runs = BestRun("label_permuted_mnist", "area", "fully_connected_tanh", ["sgd", "upgdv2_normalized"]).get_best_run()
 
     print(best_runs)
-    plotter = Plotter(best_runs, task_name="summer_with_signals_change")
+    plotter = Plotter(best_runs, task_name="mnist")
     plotter.plot()
-    # plotter.plot_1st_n_tasks(n_tasks=5)
-    # plotter.plot_last_n_tasks(n_tasks=5)
+    plotter.plot_1st_n_tasks(n_tasks=5)
+    plotter.plot_last_n_tasks(n_tasks=5)
