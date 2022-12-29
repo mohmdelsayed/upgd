de4b7a720db4563d30c65b0b888372107852c03a
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 19:10:23 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author="Mohamed Elsayed",
     author_email="mohamedelsayed@ualberta.ca",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=['backpack-for-pytorch==1.3.0'],
+    install_requires=['backpack-for-pytorch==1.3.0', 'HesScale==1.0.0'],
 )
