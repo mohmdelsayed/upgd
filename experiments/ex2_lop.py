4c76fd7aed6653c3ccf3820d2b12ab69cac616bf
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 21:36:04 -0700

    add search optimizer to all tasks

--- experiments/ex2_lop.py
+++ experiments/ex2_lop.py
@@ -1,5 +1,6 @@
 from core.grid_search import GridSearch
 from core.learner.upgd import UPGDv2NormalizedLearnerFO, UPGDv2NormalizedLearnerSO
+from core.learner.search import SearchLearnerNormalFO, SearchLearnerNormalSO, SearchLearnerAntiCorrFO, SearchLearnerAntiCorrSO
 from core.learner.sgd import SGDLearner
 from core.task.summer_with_signals_change import SummerWithSignalsChange
 from core.network.fcn_tanh import FullyConnectedTanh
@@ -31,6 +32,46 @@ grids = [
         network=[FullyConnectedTanh()],
         n_samples=[500000],
     ),
+    GridSearch(
+        seed=[i for i in range(0, 2)],
+        # lr=[10 ** -i for i in range(0, 3)],
+        lr=[0.01],
+        beta_utility=[0.0],
+        temp=[1.0],
+        sigma=[1.0],
+        network=[FullyConnectedTanh()],
+        n_samples=[500000],
+    ),
+    GridSearch(
+        seed=[i for i in range(0, 2)],
+        # lr=[10 ** -i for i in range(0, 3)],
+        lr=[0.01],
+        beta_utility=[0.0],
+        temp=[1.0],
+        sigma=[1.0],
+        network=[FullyConnectedTanh()],
+        n_samples=[500000],
+    ),
+    GridSearch(
+        seed=[i for i in range(0, 2)],
+        # lr=[10 ** -i for i in range(0, 3)],
+        lr=[0.01],
+        beta_utility=[0.0],
+        temp=[1.0],
+        sigma=[1.0],
+        network=[FullyConnectedTanh()],
+        n_samples=[500000],
+    ),
+    GridSearch(
+        seed=[i for i in range(0, 2)],
+        # lr=[10 ** -i for i in range(0, 3)],
+        lr=[0.01],
+        beta_utility=[0.0],
+        temp=[1.0],
+        sigma=[1.0],
+        network=[FullyConnectedTanh()],
+        n_samples=[500000],
+    ),
     GridSearch(seed=[i for i in range(0, 2)],
             #    lr=[10 ** -i for i in range(0, 3)],
                lr=[0.01],
@@ -42,6 +83,10 @@ grids = [
 learners = [
     UPGDv2NormalizedLearnerFO(FullyConnectedTanh(), dict()),
     UPGDv2NormalizedLearnerSO(FullyConnectedTanh(), dict()),
+    SearchLearnerNormalFO(FullyConnectedTanh(), dict()),
+    SearchLearnerNormalSO(FullyConnectedTanh(), dict()),
+    SearchLearnerAntiCorrFO(FullyConnectedTanh(), dict()),
+    SearchLearnerAntiCorrSO(FullyConnectedTanh(), dict()),
     SGDLearner(FullyConnectedTanh(), dict()),
 ]
 
