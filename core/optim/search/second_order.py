0e12e655d9044cbb0a14595ac3356d26363ed3f3
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 21:02:54 -0700

    remove old so optimizers scripts

--- core/optim/search/second_order.py
+++ core/optim/search/second_order.py
@@ -1,60 +1,16 @@
-from hesscale import HesScale
+import sys, os
+sys.path.insert(1, os.getcwd())
+from HesScale.hesscale import HesScale
 import torch
 
-
-class SecondOrderSearchNormal(torch.optim.Optimizer):
+class SecondOrderSearchAntiCorrNormalized(torch.optim.Optimizer):
     method = HesScale()
+    def __init__(self, params, lr=1e-5, beta_utility=0.0, temp=1.0, sigma=1.0):
+        defaults = dict(lr=lr, beta_utility=beta_utility, temp=temp, sigma=sigma, method_field=type(self).method.savefield,
+)
+        super(SecondOrderSearchAntiCorrNormalized, self).__init__(params, defaults)
 
-    def __init__(
-        self, params, lr=1e-5, beta_utility=0.999, beta_weight=0.999, temp=1.0
-    ):
-        defaults = dict(
-            lr=lr,
-            method_field=type(self).method.savefield,
-            beta_utility=beta_utility,
-            beta_weight=beta_weight,
-            temp=temp,
-        )
-        super(SecondOrderSearchNormal, self).__init__(params, defaults)
-
-    def step(self):
-        for group in self.param_groups:
-            for p in group["params"]:
-                state = self.state[p]
-                if len(state) == 0:
-                    state["step"] = 0
-                    state["avg_utility"] = torch.zeros_like(p.data)
-                state["step"] += 1
-                bias_correction = 1 - group["beta_utility"] ** state["step"]
-                avg_utility = state["avg_utility"]
-                hess_param = getattr(p, group["method_field"])
-                noise = torch.randn_like(p.grad)
-                utility = 0.5 * hess_param * p.data ** 2 - p.grad.data * p.data
-                avg_utility.mul_(group["beta_utility"]).add_(
-                    utility, alpha=1 - group["beta_utility"]
-                )
-                p.data.add_(
-                    noise * (1 - torch.tanh_(avg_utility / group["temp"])),
-                    alpha=-group["lr"] / bias_correction,
-                )
-
-
-class SecondOrderSearchAntiCorr(torch.optim.Optimizer):
-    method = HesScale()
-
-    def __init__(
-        self, params, lr=1e-5, beta_utility=0.999, beta_weight=0.999, temp=1.0
-    ):
-        defaults = dict(
-            lr=lr,
-            method_field=type(self).method.savefield,
-            beta_utility=beta_utility,
-            beta_weight=beta_weight,
-            temp=temp,
-        )
-        super(SecondOrderSearchAntiCorr, self).__init__(params, defaults)
-
-    def step(self):
+    def step(self, loss):
         for group in self.param_groups:
             for p in group["params"]:
                 state = self.state[p]
@@ -62,18 +18,27 @@ class SecondOrderSearchAntiCorr(torch.optim.Optimizer):
                     state["step"] = 0
                     state["avg_utility"] = torch.zeros_like(p.data)
                     state["prev_noise"] = torch.zeros_like(p.data)
+                    state["max_utility"] = torch.tensor(-torch.inf)
                 state["step"] += 1
                 bias_correction = 1 - group["beta_utility"] ** state["step"]
-                avg_utility = state["avg_utility"]
-                hess_param = getattr(p, group["method_field"])
-                new_noise = torch.randn_like(p.grad)
+                new_noise = torch.randn_like(p.grad) * group["sigma"] * torch.tanh(loss)
                 noise = new_noise - state["prev_noise"]
                 state["prev_noise"] = new_noise
+                avg_utility = state["avg_utility"]
+                hess_param = getattr(p, group["method_field"])
                 utility = 0.5 * hess_param * p.data ** 2 - p.grad.data * p.data
                 avg_utility.mul_(group["beta_utility"]).add_(
                     utility, alpha=1 - group["beta_utility"]
                 )
-                p.data.add_(
-                    noise * (1 - torch.tanh_(avg_utility / group["temp"])),
-                    alpha=-group["lr"] / bias_correction,
+                current_max = avg_utility.max()
+                if state["max_utility"] < current_max:
+                    state["max_utility"] = current_max
+                scaled_utility = torch.tanh_(
+                    (avg_utility / bias_correction)
+                    / group["temp"]
+                    / state["max_utility"]
                 )
+                p.data.add_(
+                    noise * (1 - scaled_utility), alpha=-group["lr"]
+                )
+
