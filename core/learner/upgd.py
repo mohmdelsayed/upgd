bc8a8ef3db2dfc675df0f8f4deaeab292865300d
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-28 21:00:51 -0700

    update upgd so learner

--- core/learner/upgd.py
+++ core/learner/upgd.py
@@ -1,9 +1,16 @@
 from core.learner.learner import Learner
 from core.optim.gt.first_order import FirstOrderUPGDv2AntiCorrNormalized
+from core.optim.gt.second_order import SecondOrderUPGDv2AntiCorrNormalized
 
 
-class UPGDv2NormalizedLearner(Learner):
+class UPGDv2NormalizedLearnerFO(Learner):
     def __init__(self, network, optim_kwargs):
         optimizer = FirstOrderUPGDv2AntiCorrNormalized
-        name = "upgdv2_normalized"
+        name = "upgdv2_normalized_fo"
+        super().__init__(name, network, optimizer, optim_kwargs)
+
+class UPGDv2NormalizedLearnerSO(Learner):
+    def __init__(self, network, optim_kwargs):
+        optimizer = SecondOrderUPGDv2AntiCorrNormalized
+        name = "upgdv2_normalized_so"
         super().__init__(name, network, optimizer, optim_kwargs)
