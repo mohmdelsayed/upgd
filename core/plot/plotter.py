168c8161ed7f6e986bcdcc5b19f2eebbb5865d8f
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-30 20:30:28 -0700

    refactor plotter

--- core/plot/plotter.py
+++ core/plot/plotter.py
@@ -3,34 +3,13 @@ import matplotlib.pyplot as plt
 from core.best_run import BestRun
 import os
 import numpy as np
+from plotter_static import PlotterStatic
 
-class Plotter:
+class Plotter(PlotterStatic):
     def __init__(self, best_runs_path, task_name, avg_interval=100):
         self.best_runs_path = best_runs_path
         self.avg_interval = avg_interval
         self.task_name = task_name
-
-    def plot(self):
-        for subdir in self.best_runs_path:
-            seeds = os.listdir(f'{subdir}')
-            configuration_list = []
-            for seed in seeds:
-                with open(f"{subdir}/{seed}") as json_file:
-                    data = json.load(json_file)
-                    configuration_list.append(data["losses"])
-                    learner_name = data["learner"]
-
-            configuration_list = np.array(configuration_list).reshape(len(seeds), len(configuration_list[0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
-            mean_list = np.array(configuration_list).mean(axis=0)
-            std_list = np.array(configuration_list).std(axis=0) / np.sqrt(len(seeds))
-            plt.plot(mean_list, label=learner_name)
-            plt.title(self.task_name)
-            plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
-            plt.legend()
-        
-        plt.xlabel(f"Task")
-        plt.ylabel("Loss")
-        plt.show()
 
     def plot_1st_n_tasks(self, n_tasks=5):
         for subdir in self.best_runs_path:
