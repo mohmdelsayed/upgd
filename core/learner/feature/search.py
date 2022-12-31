a9aea313062cd5acbec849ddb4a5c128888a0b63
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2022-12-30 19:16:55 -0700

    add feature learners

--- core/learner/feature/search.py
+++ core/learner/feature/search.py
@@ -1,8 +1,8 @@
 from core.learner.learner import Learner
 from core.optim.feature.search.first_order import FirstOrderSearchAntiCorrNormalized
 
-class FeatureUPGDv2LearnerFONormalized(Learner):
+class FeatureSearchLearnerNormalFONormalized(Learner):
     def __init__(self, network, optim_kwargs):
         optimizer = FirstOrderSearchAntiCorrNormalized
-        name = "feature_upgdv2_fo_anticorr_normalized"
+        name = "feature_search_fo_anticorr_normalized"
         super().__init__(name, network, optimizer, optim_kwargs)
