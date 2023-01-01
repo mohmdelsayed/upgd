fc2b909c74bc33be9f6c58203ed1d3c57599a8c9
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-01-01 15:27:45 -0700

    update sbatch script

--- core/utils.py
+++ core/utils.py
@@ -219,14 +219,9 @@ do
 echo \"#!/bin/bash\" > ${{f%.*}}.sh
 echo -e \"#SBATCH --signal=USR1@90\" >> ${{f%.*}}.sh
 echo -e \"#SBATCH --job-name=\"${{f%.*}}\"\\t\\t\\t# single job name for the array\" >> ${{f%.*}}.sh
-echo -e \"#SBATCH --mem=4G\\t\\t\\t# maximum memory 100M per job\" >> ${{f%.*}}.sh
-echo -e \"#SBATCH --time=00:30:00\\t\\t\\t# maximum wall time per job in d-hh:mm or hh:mm:ss\" >> ${{f%.*}}.sh
-if [[ \"${{f%.*}}\" != *'SGD'* ]]; then
+echo -e \"#SBATCH --mem=2G\\t\\t\\t# maximum memory 100M per job\" >> ${{f%.*}}.sh
+echo -e \"#SBATCH --time=01:00:00\\t\\t\\t# maximum wall time per job in d-hh:mm or hh:mm:ss\" >> ${{f%.*}}.sh
 echo \"#SBATCH --array=1-240\" >> ${{f%.*}}.sh
-else
-echo \"#SBATCH --array=1-40\" >> ${{f%.*}}.sh
-fi
-echo -e \"#SBATCH --gres=gpu:1\\t\\t\\t# Number of GPUs (per node)\" >> ${{f%.*}}.sh
 echo -e \"#SBATCH --account=def-ashique\" >> ${{f%.*}}.sh
 
 echo "cd \"../../\"" >> ${{f%.*}}.sh
