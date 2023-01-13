d649ed9ac3a495442724aca087bf722fdea31efa
Mohamed Elsayed <moh@beluga4.int.ets1.calculquebec.ca>
2023-01-13 15:53:46 -0500

    update plotter

--- core/plot/plotter.py
+++ core/plot/plotter.py
@@ -4,12 +4,43 @@ from core.best_run import BestRun
 import os
 import numpy as np
 from plotter_static import PlotterStatic
+import matplotlib
+matplotlib.rcParams['axes.spines.right'] = False
+matplotlib.rcParams['axes.spines.top'] = False
+matplotlib.rcParams["axes.spines.right"] = False
+matplotlib.rcParams["axes.spines.top"] = False
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
+matplotlib.rcParams.update({'font.size': 12})
 
-class Plotter(PlotterStatic):
+class Plotter:
     def __init__(self, best_runs_path, task_name, avg_interval=100):
         self.best_runs_path = best_runs_path
         self.avg_interval = avg_interval
         self.task_name = task_name
+
+    def plot(self):
+        for subdir in self.best_runs_path:
+            seeds = os.listdir(f'{subdir}')
+            configuration_list = []
+            for seed in seeds:
+                with open(f"{subdir}/{seed}") as json_file:
+                    data = json.load(json_file)
+                    configuration_list.append(data["losses"])
+                    learner_name = data["learner"]
+
+            configuration_list = np.array(configuration_list).reshape(len(seeds), len(configuration_list[0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
+            mean_list = np.array(configuration_list).mean(axis=0)
+            std_list = np.array(configuration_list).std(axis=0) / np.sqrt(len(seeds))
+            plt.plot(mean_list, label=learner_name)
+            plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
+            plt.legend()
+        
+        plt.xlabel(f"Task")
+        plt.ylabel("Loss")
+        plt.ylim(bottom=0.0)
+        plt.savefig("avg_losses.pdf", bbox_inches='tight')
+        plt.clf()
 
     def plot_1st_n_tasks(self, n_tasks=5):
         for subdir in self.best_runs_path:
@@ -24,13 +55,14 @@ class Plotter(PlotterStatic):
             mean_list = np.array(configuration_list).mean(axis=0)[0:n_tasks * self.avg_interval-1]
             std_list = np.array(configuration_list).std(axis=0)[0:n_tasks * self.avg_interval-1] / np.sqrt(len(seeds))
             plt.plot(mean_list, label=learner_name)
-            plt.title(f"First {n_tasks} tasks {self.task_name}")
             plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
             plt.legend()
         
-        plt.xlabel(f"Time Step")
+        plt.xlabel(f"Sample")
         plt.ylabel("Loss")
-        plt.show()
+        plt.ylim(bottom=0.0, top=3.0)
+        plt.savefig("first_n_tasks.pdf", bbox_inches='tight')
+        plt.clf()
 
     def plot_last_n_tasks(self, n_tasks=5):
         for subdir in self.best_runs_path:
@@ -46,13 +78,15 @@ class Plotter(PlotterStatic):
             mean_list = all_mean_list[(-n_tasks * self.avg_interval):]
             std_list = np.array(configuration_list).std(axis=0)[(-n_tasks * self.avg_interval):] / np.sqrt(len(seeds))
             plt.plot(np.arange(n_tasks * self.avg_interval) + len(np.array(all_mean_list))  - n_tasks * self.avg_interval, mean_list, label=learner_name)
-            plt.title(f"Last {n_tasks} tasks {self.task_name}")
+            plt.ylim(bottom=0.0, top=3.0)
             plt.fill_between(np.arange(n_tasks * self.avg_interval) + len(np.array(all_mean_list))  - n_tasks * self.avg_interval, mean_list - std_list, mean_list + std_list, alpha=0.1)
             plt.legend()
         
-        plt.xlabel(f"Time Step")
+        plt.xlabel(f"Sample")
         plt.ylabel("Loss")
-        plt.show()
+        plt.ylim(bottom=0.0, top=3.0)
+        plt.savefig("last_n_tasks.pdf", bbox_inches='tight')
+        plt.clf()
 
 
 if __name__ == "__main__":
