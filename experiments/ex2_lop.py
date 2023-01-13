b22c4c6aa4a3aca5c9488334f4d481c309aa408f
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-01-13 15:26:12 -0500

    update PermutedAdder task

--- experiments/ex2_lop.py
+++ experiments/ex2_lop.py
@@ -18,16 +18,6 @@ from core.learner.weight.upgd import (
     UPGDv1LearnerSONormalMax,
 )
 
-from core.learner.weight.search import (
-    SearchLearnerAntiCorrFONormalized,
-    SearchLearnerAntiCorrSONormalized,
-    SearchLearnerAntiCorrFOMax,
-    SearchLearnerAntiCorrSOMax,
-    SearchLearnerNormalFONormalized,
-    SearchLearnerNormalSONormalized,
-    SearchLearnerNormalFOMax,
-    SearchLearnerNormalSOMax,
-)
 
 from core.learner.sgd import SGDLearner
 from core.learner.anti_pgd import AntiPGDLearner
@@ -43,29 +33,30 @@ exp_name = "ex2_lop_summer_with_signals_change"
 task = tasks[exp_name]()
 
 gt_grids = GridSearch(
-        seed=[i for i in range(0, 30)],
-        lr=[2 ** -i for i in range(1, 9)],
-        beta_utility=[0.0],
-        temp=[1.0],
-        sigma=[2.0, 1.0, 0.5, 0.25,],
-        network=[FullyConnectedTanh(), FullyConnectedReLU(), FullyConnectedLeakyReLU()],
-        n_samples=[500000],
+               seed=[i for i in range(0, 30)],
+               lr=[10 ** -i for i in range(0, 5)],
+               beta_utility=[0.0],
+               temp=[1.0],
+               sigma=[1.0],
+               network=[FullyConnectedTanh(), FullyConnectedReLU(), FullyConnectedLeakyReLU()],
+               n_samples=[250000],
+               noise_damping=[0],
     )
 
 pgd_grids = GridSearch(
                seed=[i for i in range(0, 30)],
-               lr=[2 ** -i for i in range(1, 9)],
-               sigma=[2.0, 1.0, 0.5, 0.25,],
+               lr=[10 ** -i for i in range(0, 5)],
+               sigma=[1.0],
                network=[FullyConnectedTanh(), FullyConnectedReLU(), FullyConnectedLeakyReLU()],
-               n_samples=[500000],
+               n_samples=[250000],
     )
 
 
 sgd_grid = GridSearch(
                seed=[i for i in range(0, 30)],
-               lr=[2 ** -i for i in range(1, 9)],
+               lr=[10 ** -i for i in range(0, 5)],
                network=[FullyConnectedTanh(), FullyConnectedReLU(), FullyConnectedLeakyReLU()],
-               n_samples=[500000],
+               n_samples=[250000],
     )
 
 # gt_grids = GridSearch(
@@ -94,17 +85,9 @@ sgd_grid = GridSearch(
 #     )
 
 
-grids = [gt_grids for _ in range(24)] + [sgd_grid] + [pgd_grids for _ in range(2)] 
+grids = [gt_grids for _ in range(16)] + [sgd_grid] + [pgd_grids for _ in range(2)] 
 
 learners = [
-    SearchLearnerAntiCorrFONormalized(),
-    SearchLearnerAntiCorrSONormalized(),
-    SearchLearnerAntiCorrFOMax(),
-    SearchLearnerAntiCorrSOMax(),
-    SearchLearnerNormalFONormalized(),
-    SearchLearnerNormalSONormalized(),
-    SearchLearnerNormalFOMax(),
-    SearchLearnerNormalSOMax(),
     UPGDv2LearnerFOAntiCorrNormalized(),
     UPGDv2LearnerSOAntiCorrNormalized(),
     UPGDv1LearnerFOAntiCorrNormalized(),
