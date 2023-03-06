823ee01347b82d79b5c4dde49f4a3abd6d76e0f6
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-03-06 14:01:34 -0700

    add linear networks

--- core/network/fcn_linear.py
+++ core/network/fcn_linear.py
@@ -31,6 +31,48 @@ class FullyConnectedLinear(nn.Sequential):
     def __str__(self):
         return self.name
 
+class SmallFullyConnectedLinearGates(nn.Sequential):
+    def __init__(self, n_obs=10, n_outputs=10, n_hidden_units=50):
+        super(SmallFullyConnectedLinearGates, self).__init__()
+        self.name = "small_fully_connected_linear_gates"
+        self.add_module("linear_1", nn.Linear(in_features=n_obs, out_features=n_hidden_units))
+        self.add_module("gate_1", GateLayer(n_hidden_units))
+        self.add_module("linear_2", nn.Linear(in_features=n_hidden_units, out_features=n_hidden_units))
+        self.add_module("gate_2", GateLayer(n_hidden_units))
+        self.add_module("linear_3", nn.Linear(in_features=n_hidden_units, out_features=n_outputs))
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.reset_parameters()
+
+    def __str__(self):
+        return self.name
+    
+class SmallFullyConnectedLinear(nn.Sequential):
+    def __init__(self, n_obs=10, n_outputs=10, n_hidden_units=50):
+        super(SmallFullyConnectedLinear, self).__init__()
+        self.name = "small_fully_connected_linear"
+        self.add_module("linear_1", nn.Linear(in_features=n_obs, out_features=n_hidden_units))
+        self.add_module("linear_2", nn.Linear(in_features=n_hidden_units, out_features=n_hidden_units))
+        self.add_module("linear_3", nn.Linear(in_features=n_hidden_units, out_features=n_outputs))
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.reset_parameters()
+
+    def __str__(self):
+        return self.name
+
+class LinearLayer(nn.Sequential):
+    def __init__(self, n_obs=10, n_outputs=10):
+        super(LinearLayer, self).__init__()
+        self.name = "linear_layer"
+        self.add_module("linear_1", nn.Linear(in_features=n_obs, out_features=n_outputs))
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.reset_parameters()
+
+    def __str__(self):
+        return self.name
+
 if __name__ == "__main__":
     net = FullyConnectedLinear()
     print(net)
