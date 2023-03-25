df774816e0a1a4ca1aeea42afd63273d2f5cdc85
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-03-25 16:45:04 -0400

    update stationary mnist to emnist

--- core/task/stationary_mnist.py
+++ core/task/stationary_mnist.py
@@ -13,7 +13,7 @@ class StationaryMNIST(Task):
         self.dataset = self.get_dataset(True)
         self.step = 0
         self.n_inputs = 784
-        self.n_outputs = 10
+        self.n_outputs = 47
         self.criterion = "cross_entropy"
         super().__init__(name, batch_size)
 
@@ -31,10 +31,11 @@ class StationaryMNIST(Task):
         return iter(self.get_dataloader(self.dataset))
 
     def get_dataset(self, train=True):
-        return torchvision.datasets.MNIST(
+        return torchvision.datasets.EMNIST(
             "dataset",
             train=train,
             download=True,
+            split="balanced",
             transform=torchvision.transforms.Compose(
                 [
                     torchvision.transforms.ToTensor(),
