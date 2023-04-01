7a23d2e9ac0a190d873eba756188b14d697352ba
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-03-31 23:55:16 -0400

    update static plotter

--- core/plot/plotter_static.py
+++ core/plot/plotter_static.py
@@ -13,7 +13,7 @@ matplotlib.rcParams["ps.fonttype"] = 42
 matplotlib.rcParams.update({'font.size': 12})
 
 class PlotterStatic:
-    def __init__(self, best_runs_path, task_name, avg_interval=100):
+    def __init__(self, best_runs_path, task_name, avg_interval=1000):
         self.best_runs_path = best_runs_path
         self.avg_interval = avg_interval
         self.task_name = task_name
@@ -25,7 +25,7 @@ class PlotterStatic:
             for seed in seeds:
                 with open(f"{subdir}/{seed}") as json_file:
                     data = json.load(json_file)
-                    configuration_list.append(data["losses"])
+                    configuration_list.append(data["accuracies"])
                     learner_name = data["learner"]
 
             configuration_list = np.array(configuration_list).reshape(len(seeds), len(configuration_list[0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
@@ -33,17 +33,17 @@ class PlotterStatic:
             std_list = np.array(configuration_list).std(axis=0) / np.sqrt(len(seeds))
             plt.plot(mean_list, label=learner_name)
             plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.2)
-            plt.ylim([0.0, 1.5])
+            plt.ylim([0.0, 1.0])
             plt.legend()
         
         plt.xlabel(f"Bin ({self.avg_interval} sample each)")
-        plt.ylabel("Loss")
+        plt.ylabel("Accuracy")
         plt.savefig("avg_losses.pdf", bbox_inches='tight')
         plt.clf()
 
 
 if __name__ == "__main__":
-    best_runs = BestRun("ex6_static_mnist", "area", "fully_connected_tanh", ["sgd", "upgdv2_normalized_so"]).get_best_run()
+    best_runs = BestRun("ex5_stationary_mnist", "area", "fully_connected_relu", ["sgd"]).get_best_run(measure="accuracies")
     print(best_runs)
     plotter = PlotterStatic(best_runs, task_name="mnist")
     plotter.plot()
