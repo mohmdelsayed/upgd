5f3283dc2fede9a194829e4a69351a9ff41e7d0b
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-04-22 12:41:16 -0600

    add MobileNetv2 for output-permuted CIFAR-100 task

--- core/utils.py
+++ core/utils.py
@@ -9,6 +9,7 @@ from core.task.utility_task import UtilityTask
 
 from core.network.fcn_leakyrelu import FullyConnectedLeakyReLU, SmallFullyConnectedLeakyReLU, FullyConnectedLeakyReLUGates, SmallFullyConnectedLeakyReLUGates
 from core.network.fcn_relu import FullyConnectedReLU, SmallFullyConnectedReLU, FullyConnectedReLUGates, SmallFullyConnectedReLUGates, ConvolutionalNetworkReLU
+from core.network.mobilenetv2 import MobileNetV2
 from core.network.fcn_tanh import FullyConnectedTanh, SmallFullyConnectedTanh, FullyConnectedTanhGates, SmallFullyConnectedTanhGates
 from core.network.fcn_linear import FullyConnectedLinear, FullyConnectedLinearGates, LinearLayer, SmallFullyConnectedLinear, SmallFullyConnectedLinearGates
 
@@ -91,6 +92,7 @@ networks = {
     "small_fully_connected_relu_gates": SmallFullyConnectedReLUGates,
     "small_fully_connected_linear_gates": SmallFullyConnectedLinearGates,
     "convolutional_network_relu": ConvolutionalNetworkReLU,
+    "mobilenetv2": MobileNetV2,
 }
 
 learners = {
--- experiments/ex9_label_permuted_cifar100.py
+++ experiments/ex9_label_permuted_cifar100.py
@@ -1,20 +1,16 @@
 from core.grid_search import GridSearch
 from core.learner.weight.upgd import (
     UPGDv2LearnerFONormalNormalized,
-    UPGDv2LearnerSONormalNormalized,
     UPGDv1LearnerFONormalNormalized,
-    UPGDv1LearnerSONormalNormalized,
     UPGDv2LearnerFONormalMax,
-    UPGDv2LearnerSONormalMax,
     UPGDv1LearnerFONormalMax,
-    UPGDv1LearnerSONormalMax,
 )
 
 
 from core.learner.sgd import SGDLearner
 from core.learner.pgd import PGDLearner
 from core.learner.shrink_and_perturb import ShrinkandPerturbLearner
-from core.network.fcn_relu import ConvolutionalNetworkReLU
+from core.network.mobilenetv2 import MobileNetV2
 from core.runner import Runner
 from core.run.run import Run
 from core.utils import create_script_generator, create_script_runner, tasks
@@ -28,7 +24,7 @@ up_grids = GridSearch(
                beta_utility=[0.999, 0.9999],
                sigma=[0.01, 0.1, 1.0],
                weight_decay=[0.0, 0.1, 0.01, 0.001],
-               network=[ConvolutionalNetworkReLU()],
+               network=[MobileNetV2()],
                n_samples=[1000000],
     )
 
@@ -36,7 +32,7 @@ pgd_grids = GridSearch(
                seed=[i for i in range(0, 10)],
                lr=[10 ** -i for i in range(2, 6)],
                sigma=[0.005, 0.05, 0.5],
-               network=[ConvolutionalNetworkReLU()],
+               network=[MobileNetV2()],
                n_samples=[1000000],
     )
 
@@ -44,7 +40,7 @@ pgd_grids = GridSearch(
 sgd_grid = GridSearch(
                seed=[i for i in range(0, 10)],
                lr=[10 ** -i for i in range(2, 6)],
-               network=[ConvolutionalNetworkReLU()],
+               network=[MobileNetV2()],
                n_samples=[1000000],
     )
 
@@ -53,7 +49,7 @@ sp_grid = GridSearch(
                lr=[10 ** -i for i in range(2, 6)],
                sigma=[0.005, 0.05, 0.5],
                decay=[0.1, 0.01, 0.001],
-               network=[ConvolutionalNetworkReLU()],
+               network=[MobileNetV2()],
                n_samples=[1000000],
     )
 
