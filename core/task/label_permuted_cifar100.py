7b536fe1bbb60cd75e7f2dbb1811c177942e35cf
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-04-30 15:30:59 -0600

    update cifar100 task: preprocess data using resnet

--- core/task/label_permuted_cifar100.py
+++ core/task/label_permuted_cifar100.py
@@ -1,13 +1,21 @@
 import torch
 import torchvision
 from .task import Task
+from PIL import Image
+import pickle
 
+class CustomCIFAR100(torchvision.datasets.CIFAR100):
+    def __init__(self, *args, **kwargs):
+        super(CustomCIFAR100, self).__init__(*args, **kwargs)
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        return img, target
 
 class LabelPermutedCIFAR100(Task):
     """
     Iteratable CIFAR-100 task with permuted labels.
-    Each sample is a 32x32x3 image and the label is a number between 0 and 100.
-    The labels are permuted every 10000 steps.
+    Each sample is a 1000-dimensional resnet50-processed image and the label is a number between 0 and 99.
+    The labels are permuted every 5000 steps.
     """
 
     def __init__(self, name="label_permuted_cifar100", batch_size=1, change_freq=5000):
@@ -17,10 +25,6 @@ class LabelPermutedCIFAR100(Task):
         self.n_inputs = 1000
         self.n_outputs = 100
         self.criterion = "cross_entropy"
-        self.feature_extractor = torchvision.models.resnet18(pretrained=True)
-        for param in self.feature_extractor.parameters():
-            param.requires_grad = False
-        self.feature_extractor.eval()
         super().__init__(name, batch_size)
 
     def __next__(self):
@@ -40,17 +44,36 @@ class LabelPermutedCIFAR100(Task):
         return iter(self.get_dataloader(self.dataset))
 
     def get_dataset(self, train=True):
-        return torchvision.datasets.CIFAR100(
+        dataset= CustomCIFAR100(
             "dataset",
             train=train,
-            download=True,
+            download=True
+        )
+        # check if the dataset is already processed
+        file_name = 'processed_cifar100.pkl'
+        try:
+            with open(file_name, 'rb') as f:
+                dataset.data = pickle.load(f)
+            return dataset
+        except:
             transform=torchvision.transforms.Compose(
                 [
                     torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Lambda(lambda x: self.feature_extractor(x.unsqueeze(0)).squeeze(0)),
+                    torchvision.transforms.Normalize((0.5,), (0.5,), (0.5,)),
                 ]
-            ),
-        )
+            )
+            images = [transform(Image.fromarray(img)) for img in dataset.data]
+            # stack all images into a single tensor
+            dataset.data = torch.stack(images)
+            resnet = torchvision.models.resnet50(pretrained=True)
+            for param in resnet.parameters():
+                param.requires_grad_(False)
+            resnet.eval()
+            dataset.data = resnet(dataset.data)
+            # save the processed dataset
+            with open(file_name, 'wb') as f:
+                pickle.dump(dataset.data, f)
+            return dataset
 
     def get_dataloader(self, dataset):
         return torch.utils.data.DataLoader(
