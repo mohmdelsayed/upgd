da6c42685f4ae4524e8974d63df470b6664325b3
Moahmed Elsayed <mohamed.elsayed.1995@gmail.com>
2023-08-18 20:46:49 -0600

    update utility plotter

--- core/plot/plotter_utility.py
+++ core/plot/plotter_utility.py
@@ -10,7 +10,7 @@ matplotlib.rcParams["axes.spines.right"] = False
 matplotlib.rcParams["axes.spines.top"] = False
 matplotlib.rcParams["pdf.fonttype"] = 42
 matplotlib.rcParams["ps.fonttype"] = 42
-matplotlib.rcParams.update({'font.size': 12})
+matplotlib.rcParams.update({'font.size': 14})
 
 class UtilityPlotter:
     def __init__(self, best_runs_path, avg_interval=10):
@@ -28,34 +28,34 @@ class UtilityPlotter:
                     configuration_list.append(data["losses"])
                     learner_name = data["learner"]
                     for measure in data[corr_name]:
-                        if "nvidia" in measure:
-                            continue
                         if measure not in util_correlations:
                             util_correlations[measure] = []
                         util_correlations[measure].append(data[corr_name][measure])
-
             configuration_list = np.array(configuration_list).reshape(len(seeds), len(configuration_list[0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
 
             mean_list = np.array(configuration_list).mean(axis=0)
             std_list = np.array(configuration_list).std(axis=0) / np.sqrt(len(seeds))
 
+            x = [self.avg_interval * i for i in range(len(mean_list))]
+            plt.plot(x, mean_list, label=learner_name)
+            plt.fill_between(x, mean_list - std_list, mean_list + std_list, alpha=0.1)
 
-            plt.plot(mean_list, label=learner_name)
-            plt.fill_between(range(len(mean_list)), mean_list - std_list, mean_list + std_list, alpha=0.1)
-            plt.legend()
+            # plt.legend()
 
-            for measure in util_correlations:
+            colors = ["tab:orange", "tab:green", "tab:red", "tab:brown"]
+            x = [self.avg_interval * i for i in range(len(mean_list))]
+            for color, measure in zip(colors, util_correlations):
                 util_correlations[measure] = np.array(util_correlations[measure]).reshape(len(seeds), len(util_correlations[measure][0]) // self.avg_interval, self.avg_interval).mean(axis=-1)
 
                 mean_measure = np.array(util_correlations[measure]).mean(axis=0)
                 std_measure = np.array(util_correlations[measure]).std(axis=0) / np.sqrt(len(seeds))
-                plt.plot(mean_measure, label=f"{measure}")
-                plt.fill_between(range(len(mean_measure)), mean_measure - std_measure, mean_measure + std_measure, alpha=0.1)
-                plt.legend()
+                plt.plot(x, mean_measure, label=f"{measure}", linewidth=2, color=color)
+                plt.fill_between(x, mean_measure - std_measure, mean_measure + std_measure, alpha=0.1, color=color)
+                # plt.legend()
 
         
-        plt.xlabel(f"Bin ({self.avg_interval} samples each)")
-        plt.ylabel("Spearman's Coefficient")
+        plt.xlabel("Number of Samples", fontsize=20)
+        plt.ylabel("Spearman's Coefficient", fontsize=20)
         plt.savefig(f"{corr_name}.pdf", bbox_inches='tight')
         plt.clf()
 
